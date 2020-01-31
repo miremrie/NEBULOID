38f243fe183cc3065a1ab7939dc65118199374ab
Univerzitet Metropolitan <met@bmu.internal>
2020-01-31 23:15:53 +0100

    Ship Movement

--- Assets/ProcGen.cs
+++ Assets/ProcGen.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : MonoBehaviour
+public class ProcGen : MonoBehaviour
 {
-	Transform ship;
+    GameObject obstaclePrefab;
+    GameObject fuelPrefab;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ Assets/ShipMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    public Transform ship;
+    public float animTime;
+    private Timer leftTimer;
+    private Timer rightTimer;
+    public Transform leftPivot;
+    public Transform rightPivot;
+    public float rotateSpeed;
+    public float moveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftTimer = new Timer(animTime);
+        rightTimer = new Timer(animTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftTimer.Update(Time.deltaTime);
+        rightTimer.Update(Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Rotate(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Rotate(false);
+        }
+
+        MovementUpdate();
+      
+    }
+
+    void MovementUpdate() {
+
+        if (leftTimer.IsRunning() && rightTimer.IsRunning())
+        {
+            ship.Translate(ship.up * moveSpeed * Time.deltaTime, Space.World);
+        }
+        else if (leftTimer.IsRunning())
+        {
+            ship.RotateAround(leftPivot.position, Vector3.forward, rotateSpeed * Time.deltaTime);
+        }
+        else if (rightTimer.IsRunning())
+        {
+            ship.RotateAround(rightPivot.position, Vector3.forward, -rotateSpeed * Time.deltaTime);
+        }
+    }
+
+    private void Rotate(bool left)
+    {
+        if (left) leftTimer.Start();
+        else rightTimer.Start();
+      
+    }
+}
+
+class Timer {
+
+    private float time;
+    private float currTime;
+    
+    public Timer(float time)
+    {
+        this.time = time;
+        currTime = time;
+    }
+
+    public void Update(float dt)
+    {
+        currTime += dt;
+    }
+
+    public bool IsRunning() {
+
+        return (currTime < time);
+    }
+
+    public void Start()
+    {
+        currTime = 0f;
+    }
+
+}
