b12bb146899b351be9d1ffe6fc792588279aff32
Univerzitet Metropolitan <met@bmu.internal>
2020-02-01 13:13:49 +0100

    Working on sonar

---
+++ Assets/Scripts/ActionObjects/SonarAction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarAction : ActionObject
+{
+    public Transform SonarMovingLightPivot;
+    public Light SonarMovingLight;
+    public Light SonarCircleLight;
+    private Timer sonarMoveTimer;
+    public float sonarMoveTime;
+    private bool sonarStarted;
+
+    private void Awake()
+    {
+        sonarMoveTimer = new Timer(sonarMoveTime);
+    }
+
+    private void Update()
+    {
+        if (sonarStarted )
+        {
+            if (!sonarMoveTimer.IsRunning())
+            {
+                sonarStarted = false;
+                SonarMovingLight.enabled = false;
+            }
+            sonarMoveTimer.Update(Time.deltaTime);
+            SonarMovingLightPivot.transform.Rotate(0,0,2* 360 * Time.deltaTime / sonarMoveTime);
+        }
+    }
+
+    public override bool IsActionObjectReady()
+    {
+        return base.IsActionObjectReady() && !sonarStarted;
+    }
+    public override void DoAction(InputController controller)
+    {
+        SonarMovingLight.enabled = true;
+        sonarStarted = true;
+        sonarMoveTimer.Start();
+    }
+
+
+}
