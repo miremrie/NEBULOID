8a8fdb8811418104a789b94685a6278b9fc5ab7e
Univerzitet Metropolitan <met@bmu.internal>
2020-02-02 02:05:42 +0100

    Parallax

---
+++ Assets/Scripts/ParalaxTexture.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxTexture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float scrollSpeed = 0.5f;
+    Renderer rend;
+    public float factor;
+
+    public Transform target;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        var p = target.position;
+        var pos = new Vector3(p.x, p.y, transform.position.z);
+        transform.position = pos;
+
+        float offsetX = pos.x;
+        float offsetY = pos.y;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(factor * offsetX, factor * offsetY));
+    }
+
+}
